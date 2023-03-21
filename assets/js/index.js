e982d6a34c5978ed93a18601237c82c623d4275b
Erika Machado <erikafirmino.ads@gmail.com>
2023-03-21 19:03:16 -0300

    atualização pg inicial e pg sobre

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,17 @@ function clickMenu(){
         menu.style.display = 'block'
     }
 }
+
+function dataAtual(){
+    let msg = document.getElementById('msg');
+    let data = new Date();
+    let hora = data.getHours();
+
+    if(hora >= 0 && hora < 12){
+        msg.innerHTML = `Agora são ${hora} horas. Bom Dia!`
+    } else if(hora >= 12 && hora < 18) {
+        msg.innerHTML = `Agora são ${hora} horas. Boa Tarde!`;
+    }else{
+        msg.innerHTML = `Agora são ${hora} horas. Boa Noite!`;
+    }
+}
